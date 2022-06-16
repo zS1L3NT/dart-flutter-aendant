099debe87c171bfa8c74568990537bf6cedcddf1
Zechariah <zechariahtan144@gmail.com>
2022-06-16 23:27:22 +0800

    ♻️ add const to home screen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:aendant/features/alert/alert.dart';
 import 'package:flutter/material.dart';
 
@@ -18,19 +16,19 @@ class HomeScreen extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              SizedBox(height: 48),
-              Text(
+              const SizedBox(height: 48),
+              const Text(
                 "Your Necklace",
                 style: TextStyle(fontSize: 36),
               ),
-              SizedBox(height: 48),
-              Text(
+              const SizedBox(height: 48),
+              const Text(
                 "BATTERY",
                 style: TextStyle(
                   fontWeight: FontWeight.w900,
                 ),
               ),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -38,8 +36,8 @@ class HomeScreen extends StatelessWidget {
                     "assets/battery.png",
                     scale: 0.5,
                   ),
-                  SizedBox(width: 16),
-                  Text(
+                  const SizedBox(width: 16),
+                  const Text(
                     "60%",
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
@@ -47,17 +45,17 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 16),
-              Text(
+              const SizedBox(height: 16),
+              const Text(
                 "STATUS",
                 style: TextStyle(
                   fontWeight: FontWeight.w900,
                 ),
               ),
-              SizedBox(height: 48),
+              const SizedBox(height: 48),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+                children: const [
                   Text(
                     "Next-Of-Kin 1",
                     style: TextStyle(
@@ -77,7 +75,7 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-          items: [
+          items: const [
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.home_outlined,
